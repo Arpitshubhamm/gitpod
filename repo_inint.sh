c154b9ef23b8f3ad3a7dee6afbc337e5e3d8cb23
Arpitshubhamm <157899697+Arpitshubhamm@users.noreply.github.com>
2024-03-10 13:11:53 +0530

    Update repo_inint.sh

--- repo_inint.sh
+++ repo_inint.sh
@@ -3,4 +3,4 @@ cd android-recovery
 git config --global user.name "Arpitshubhamm"
 git config --global user.email "namitac829@gmail.com"
 repo init --depth=1 -u https://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-12.1
-default
+
