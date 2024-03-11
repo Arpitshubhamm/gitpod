897f5bca23b27f36039228483957ed15e22cda59
Arpitshubhamm <157899697+Arpitshubhamm@users.noreply.github.com>
2024-03-11 23:24:17 +0530

    Update repint.sh

--- repint.sh
+++ repint.sh
@@ -2,5 +2,5 @@ mkdir android-recovery
 cd android-recovery
 git config --global user.name "Arpitshubhamm"
 git config --global user.email "namitac829@gmail.com"
-repo init --depth=1 -u https://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-12.1
+repo init --depth=1 -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-12.1
 
